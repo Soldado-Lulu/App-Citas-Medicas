66b0dc5f5f364c2be2199e68e842b14325507f10
Portatil <wojtekvasquez@gmail.com>
2025-10-24 15:01:58 -0400

    lo mismo que el anterior 15:01

--- backend/src/config/pg.ts
+++ backend/src/config/pg.ts
@@ -6,5 +6,7 @@ export const pg = new Pool({
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
-  ssl: (/^true$/i).test(String(process.env.PG_SSL || 'false')) ? { rejectUnauthorized: false } : undefined,
+  ssl: (/^true$/i).test(String(process.env.PG_SSL || 'false'))
+   ? { rejectUnauthorized: false }
+    : undefined,
 });
